7126fd477cb2690f5bfcfce38cd2bd78b7d7a9e8
Toshiyuki Masui <masui@pitecan.com>
2018-02-23 21:57:51 +0900

    ...

--- lib/defaultdata.rb
+++ lib/defaultdata.rb
@@ -17,17 +17,4 @@ def default_qas
   ]
 end
 
-#if $0 == __FILE__
-#  require 'test/unit'
-#  $test = true
-#end
-#
-#if defined?($test) && $test
-#  class DataTest < Test::Unit::TestCase
-#    def test_1
-#      data = defaultdata
-#      assert_equal data[:seed], 'defaultseed'
-#    end
-#  end
-#end
 
